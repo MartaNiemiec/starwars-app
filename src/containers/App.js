3ebb5fd22a6e98a2704b3eadaa12504f3ec16a96
Marta Niemiec <niemiec.marta@hotmail.com>
2019-02-25 12:43:48 +0100

    installed tachyons and segregate files

---
+++ src/containers/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+          <p>
+           STARWARS WITH API
+          </p>
+          
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
