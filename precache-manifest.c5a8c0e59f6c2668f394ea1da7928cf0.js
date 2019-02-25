2c464edc8a78535533ee09e416232dacae3ebed8
Marta Niemiec <niemiec.marta@hotmail.com>
2019-02-25 23:52:36 +0100

    Updates

---
+++ precache-manifest.c5a8c0e59f6c2668f394ea1da7928cf0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae9becc38c526d3fa70b84cde00f9948",
+    "url": "/static/media/pattern.ae9becc3.png"
+  },
+  {
+    "revision": "a43e30ecbe0c29409e920eebc645e746",
+    "url": "/static/media/STJEDISE.a43e30ec.TTF"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "aa9a13220e63349e8781",
+    "url": "/static/js/main.aa9a1322.chunk.js"
+  },
+  {
+    "revision": "049b1bc435b32bd52690",
+    "url": "/static/js/2.049b1bc4.chunk.js"
+  },
+  {
+    "revision": "aa9a13220e63349e8781",
+    "url": "/static/css/main.fe0f1424.chunk.css"
+  },
+  {
+    "revision": "049b1bc435b32bd52690",
+    "url": "/static/css/2.134f870b.chunk.css"
+  },
+  {
+    "revision": "431afcb475e8d49e50deafd9d21d8b80",
+    "url": "/index.html"
+  }
+];
