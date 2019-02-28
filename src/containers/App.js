daa2af2de6025bbe0fc2c7d9bf45dcec6ca44e50
Marta Niemiec <niemiec.marta@hotmail.com>
2019-02-28 23:53:56 +0100

    added particles and changed background

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,75 @@ import React, { Component } from 'react';
 import SearchBox from '../components/SearchBox';
 import CardList from '../components/Cardlist';
 import Scroll from '../components/Scroll';
+import Particles from 'react-particles-js';
 import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 300
+      }
+    },
+    color: {
+      value: '#ffffff'
+    },
+    shape: {
+      type: 'circle',
+      stroke: {
+        width: 0,
+        color: '#000000'
+      }
+    },
+    opacity: {
+      value: 1,
+      random: true,
+      anim: {
+        enable: true,
+        speed: 1.5,
+        opacity_min: 0,
+        sync: false
+      }
+    },
+    size: {
+      value: 2.5,
+      random: true,
+      anim: {
+        enable: false
+      }
+    },
+    line_linked: {
+      enable: false,
+    },
+    move: {
+      enable: true,
+      speed: .1,
+      direction: 'none',
+      random: true,
+      straight: false,
+      out_mode: 'out',
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 600
+      }
+    }
+  },
+  interactivity: {
+    detect_on: 'window',
+    modes: {
+      repulse: {
+        distance: 100,
+        duration: 0.4
+      }
+    }
+  },
+  retina_detect: true
+  }
+
 
 class App extends Component {
   // ====== STATE ======
@@ -22,7 +90,7 @@ class App extends Component {
     .then(films => this.setState({films: films.results}) );
   }
 
-  // With anything that comes from React, so constructor and render, are pre-built in React, any time we make our own methods on a component, we have to use arrow functions, and this makes sure that the "this" value is according to where it was created, which is the "App"
+  // With anything that comes from React, so constructor and render, are pre-built in React, any time we make our own methods on a component, we have to use arrow functions, and this makes sure that the 'this' value is according to where it was created, which is the 'App'
   onSearchChange = (event) => {   
     this.setState({ searchfield: event.target.value })
   }
@@ -31,18 +99,27 @@ class App extends Component {
     const {films, searchfield} = this.state;
     const filteredFilms = films.filter(film => {
       return film.title.toLowerCase().includes(searchfield.toLowerCase());
-  })
-  return (!films.length) ?   //if films.length==0 so it's true, !the opposite is false
-        <h1 className="f1 tc">Loading...</h1> :
-        (
-            <div className="tc">
-                <h1 className="f1">STARWARS</h1>
+    })
+    return (
+      <div>
+        <Particles 
+        className='particles' 
+        params={particlesOptions} 
+        />
+          {/*if films.length==0 so it's true, !the opposite is false */}
+        { (!films.length) 
+        ? <h1 className='f1 tc'>Loading...</h1> 
+        : (
+            <div className='tc'>
+                <h1 className='f1'>STARWARS</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <Scroll>
                   <CardList films={filteredFilms}/>
                 </Scroll>
             </div>
-        );
+          )}
+      </div>
+    )
   }
 }
 
